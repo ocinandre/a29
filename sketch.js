a010844461d569e7343e1a9a6786634cd3e881d9
ocinandre <90657849+ocinandre@users.noreply.github.com>
2021-11-13 17:14:04 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,111 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground;
+var stand1,stand2;
+var ball;
+var slingShot;
+var polygon_img;
+var backgroundImg;
+var enemy1_Img;
+function preload(){
+  backgroundImg=loadImage("GamingBackground.png")
+  polygon_img=loadImage("Superhero-01.png");
+  enemy1_Img = loadImage("Monster-01.png")
+}
+function setup() {
+  createCanvas(900,400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  ground = new Ground();
+  stand1 = new Stand(390,300,500,10);
+  stand2 = new Stand(700,200,200,10);
+ 
+  //nivel uno
+  block1 = new Block(300,275,30,40);
+  console.log(block1);
+  block2 = new Block(330,275,30,40);
+  block3 = new Block(360,275,30,40);
+  block4 = new Block(390,275,30,40);
+  block5 = new Block(420,275,30,40);
+  block6 = new Block(450,275,30,40);
+  block7 = new Block(480,275,30,40);
+  //nivel dos
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
+  //nivel tres
+  block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+  block15 = new Block(420,195,30,40);
+  //parte superior
+  block16 = new Block(390,155,30,40);
+  enemy1 = new Block(550,275,50,60)
+
+ 
+
+  ball = Bodies.circle(50,200,10);
+World.add(world,ball);
+  slingShot = new SlingShot(this.ball, {x:100, y:200});
+  
+
+}
+function draw() {
+  background(backgroundImg); 
+ 
+
+  textSize(20);
+  fill("lightyellow");
+  text("arrastra la pelota para lanzar");
+
+  ground.display();
+  stand1.display();
+  
+  strokeWeight(2);
+  stroke(15);
+  fill("skyblue");
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  fill("pink");
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  fill("turquoise");
+  block13.display();
+  block14.display();
+  block15.display();
+  enemy1.display();
+ 
+  fill("grey");
+  block16.display();
+  fill("blue")
+  imageMode(CENTER);
+  image(polygon_img, ball.position.x, ball.position.y,100,100);
+  
+  //stand2.display();
+ // ball.display();
+  slingShot.display();
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball, {x: mouseX , y: mouseY});
+
+}
+
+
+function mouseReleased(){
+  slingShot.fly();
+}
